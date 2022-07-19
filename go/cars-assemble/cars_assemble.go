93cbaf02821112cc3fd941e546234609cd881d0e
Samantha Wu <samanthayywu@gmail.com>
2022-07-19 21:41:31 +0100

    cars solution

---
+++ go/cars-assemble/cars_assemble.go
@@ -0,0 +1,24 @@
+package cars
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return float64(successRate / 100) * float64(productionRate)
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate) / 60)
+}
+
+// 10k each but 10 cars = 95k
+// So the cost for 37 cars is:
+// 3\*95,000+7\*10,000=355,000
+
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	g:= carsCount / 10 * 95000
+	r:= carsCount % 10
+	return uint(r * 10000 + g)
+}
